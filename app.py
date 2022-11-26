08951416ccd8a6814742c1f275a2c1c42ebe3721
exstoner <117363662+exstoner@users.noreply.github.com>
2022-11-26 21:52:20 +0100

    update
    don't forget to change 'YOUR TOKEN' with your actual token

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def index():
     def get_standings():
             uri = 'https://api.football-data.org/v4/competitions/2000/standings'
-            headers = { 'X-Auth-Token': '95b24a1ff13c4ad1af05c04a8434b95f' }
+            headers = { 'X-Auth-Token': 'YOUR TOKEN' }
             response = requests.get(uri, headers=headers)
             standings = response.json()["standings"]
             return standings
     def get_matches():
             uri = 'https://api.football-data.org/v4/competitions/2000/matches'
-            headers = { 'X-Auth-Token': '95b24a1ff13c4ad1af05c04a8434b95f' }
+            headers = { 'X-Auth-Token': 'YOUR TOKEN' }
             response = requests.get(uri, headers=headers)
             matches = response.json()["matches"]
             return matches
